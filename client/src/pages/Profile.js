0f2af28fcd80ee08db8e4d95ccfdd20cd051c7e5
Ed Kubiak <ek33450505@gmail.com>
2022-06-21 17:35:24 -0400

    added friend button and thought form on home and profile pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,10 +3,14 @@ import { Navigate, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 
-import { useQuery } from '@apollo/client';
+// import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -16,6 +20,8 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
 // navigate to personal profile page if username is the logged-in user's
 if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -34,12 +40,28 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+        </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -53,9 +75,10 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
             friendCount={user.friendCount}
             friends={user.friends}
           />
-  </div>
-</div>
     </div>
+  </div>
+  <div className="mb-3">{!userParam && <ThoughtForm />}</div>
+</div>
   );
 };
 
